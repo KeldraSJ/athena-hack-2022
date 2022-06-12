d10f636ce4fa7eeec3330dfdf1511d719bc3b01f
moneshadhali <99983599+moneshadhali@users.noreply.github.com>
2022-06-12 10:35:58 +0100

    calender

---
+++ 05_10/script.js
@@ -0,0 +1,91 @@
+/**
+ * Challenge: Add a new element
+ * - In JavaScript, create a new element to hold a navigation menu
+ * - Add an unordered list and a series of no less than five links to the list
+ * - Use single words like “home”, “about”, etc for the list items and set the src attribute to # for simplicity
+ * - Add the new navigation element to the DOM directly after the header
+ * - Write basic CSS and add classes as necessary to create a horizontal layout for the menu.
+ * - A tip: Use either display flex or display grid to create the horizontal menu.
+ */
+
+import Backpack from "./Backpack.js";
+
+const everydayPack = new Backpack(
+  "Everyday Backpack",
+  30,
+  "grey",
+  15,
+  26,
+  26,
+  false,
+  "December 5, 2018 15:00:00 PST",
+  "../assets/images/everyday.svg"
+);
+
+const content = `
+  
+    <figure class="backpack__image">
+      <img src=${everydayPack.image} alt="" />
+    </figure>
+    <h1 class="backpack__name">Everyday Backpack</h1>
+    <ul class="backpack__features">
+      <li class="packprop backpack__volume">Volume:<span> ${
+        everydayPack.volume
+      }l</span></li>
+      <li class="packprop backpack__color">Color:<span> ${
+        everydayPack.color
+      }</span></li>
+      <li class="backpack__age">Age:<span> ${everydayPack.backpackAge()} days old</span></li>
+      <li class="packprop backpack__pockets">Number of pockets:<span> ${
+        everydayPack.pocketNum
+      }</span></li>
+      <li class="packprop backpack__strap">Left strap length:<span> ${
+        everydayPack.strapLength.left
+      } inches</span></li>
+      <li class="packprop backpack__strap">Right strap length:<span> ${
+        everydayPack.strapLength.right
+      } inches</span></li>
+      <li class="packprop backpack__lid">Lid status:<span> ${
+        everydayPack.lidOpen
+      }</span></li>
+    </ul>
+  
+`;
+
+const content2 = `
+  <h2 class="page-header__heading">Where does the Lorem Ipsum come from?</h2>
+  <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+  The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+`;
+
+const contentNavBar = `
+      <ul>
+        <li>Home</li>
+        <li>Career</li>
+        <li>Jobs</li>
+        <li>About</li>
+        <li>Contact Us</li>
+      </ul>
+`;
+const main = document.querySelector(".maincontent");
+
+const newArticle = document.createElement("article");
+newArticle.classList.add("backpack");
+newArticle.setAttribute("id", "everyday");
+newArticle.innerHTML = content;
+
+const newArticle2 = document.createElement("article");
+newArticle2.classList.add("page-header");
+newArticle2.innerHTML = content2;
+
+const navBar = document.createElement("div");
+navBar.classList.add("navMenu");
+navBar.setAttribute("flex-direction", "row");
+navBar.innerHTML = contentNavBar;
+
+main.append(newArticle);
+main.append(newArticle2);
+
+document.querySelector("header").append(navBar);
+//main.insertBefore(navBar, main);
